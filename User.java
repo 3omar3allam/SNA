ba681ae832c89df149d06d249ad65a6c5c80f682
OthmanMohamed <33783174+OthmanMohamed@users.noreply.github.com>
2018-04-21 17:04:58 +0200

    Add files via upload

--- User.java
+++ User.java
@@ -1,11 +1,16 @@
 package sample;
 
 
+import java.io.BufferedReader;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 import static sample.usefulFunctions.*;
+enum gender{male,female}
 
 public class User {
 
@@ -13,62 +18,121 @@ public class User {
     public static ArrayList<User> allUsersID;   // Vector of all users sorted by ID
     public static ArrayList<User> allUsersName;   // Vector of all users sorted by Name
     private int ID ;                               //Unique ID for each user
-    private static int currentID=0;
-    private static Queue<Integer>  availableIDs;   //m3mlthash int 3shan tala3 error :D
-    private String Name ;
+    private gender Gender;
+    private Date birthDate;
+    public static int currentID=0;
+    public static Queue<Integer>  availableIDs;  //m3mlthash int 3shan tala3 error :D
+    // ^^ wenta btinitializeha mat3melsh = new Queue<> .. e3mel = new ArrayList<>() aw new LinkedList<>()  <allam>
+    private String FirstName;
+    private String LastName;
     private String userName;              //Unique username for login
     private LinkedList<User> Friends;     //List of all friends of the user
     private int noFriends;
 
     //******************* Constructors******************//
-    public User(String name,String username, LinkedList<User> friends, int noFriends) {
-
-        Name = name;
+    public User(String name,String username, LinkedList<User> friends, int noFriends, gender Gender, Date birthDate)
+            throws Exception {
+        StringTokenizer nameToken = new StringTokenizer(name);
+        try{
+            FirstName = nameToken.nextToken(" ");
+            LastName = nameToken.nextToken();
+        }catch(Exception ex){
+            throw new Exception("Invalid First/Last Name");
+        }
         userName=username;
         Friends = friends;
         this.noFriends = noFriends;
         if(!availableIDs.isEmpty())ID=availableIDs.remove();
         else {ID=currentID; currentID++;}
+        this.birthDate=birthDate;
+        this.Gender=Gender;
+        addToList(this); //implemented in usefulFunctions class
     }
 
-    public User(String name) {
-        Name = name;
+    public User(String name) throws Exception {
+        StringTokenizer nameToken = new StringTokenizer(name);
+        try{
+            FirstName = nameToken.nextToken(" ");
+            LastName = nameToken.nextToken();
+        }catch(Exception ex){
+            throw new Exception("Invalid First/Last Name");
+        }
         noFriends=0;
         if(!availableIDs.isEmpty())ID=availableIDs.remove();
         else {ID=currentID; currentID++;}
+        addToList(this);
 
     }
 
-    public User(String userName, String name) {
+    public User(String userName, String firstName,String lastName){
         this.userName=userName;
-        Name = name;
+        FirstName = firstName;
+        LastName = lastName;
         noFriends=0;
         if(!availableIDs.isEmpty())ID=availableIDs.remove();
         else {ID=currentID; currentID++;}
+        addToList(this);
     }
 
-    public User(String userName, String name, LinkedList<User> friends) {
+    public User(String userName, String name) throws Exception {
         this.userName=userName;
-        Name = name;
+        StringTokenizer nameToken = new StringTokenizer(name);
+        try{
+            FirstName = nameToken.nextToken(" ");
+            LastName = nameToken.nextToken();
+        }catch(Exception ex){
+            throw new Exception("Invalid First/Last Name");
+        }
+        noFriends=0;
+        if(!availableIDs.isEmpty())ID=availableIDs.remove();
+        else {ID=currentID; currentID++;}
+        addToList(this);
+    }
+
+    public User(String userName, String name, LinkedList<User> friends) throws Exception {
+        this.userName=userName;
+        StringTokenizer nameToken = new StringTokenizer(name);
+        try{
+            FirstName = nameToken.nextToken(" ");
+            LastName = nameToken.nextToken();
+        }catch(Exception ex){
+            throw new Exception("Invalid First/Last Name");
+        }
         noFriends=friends.size();
         Friends = friends;
         if(!availableIDs.isEmpty())ID=availableIDs.remove();
         else {ID=currentID; currentID++;}
+        addToList(this);
     }
 
     public User() {
         if(!availableIDs.isEmpty())ID=availableIDs.remove();
         else {ID=currentID; currentID++;}
-        Name=null;
+        FirstName=null;
+        LastName=null;
         noFriends=0;
+        addToList(this);
+    }
+
+    //*******************Copy Constructor******************//
+    public User(User copyUser)
+    {
+        if(!availableIDs.isEmpty())ID=availableIDs.remove();
+        else {ID=currentID; currentID++;}
+        FirstName=copyUser.getFirstName();
+        LastName=copyUser.getLastName();
+        Friends=copyUser.getFriends();
+        noFriends=Friends.size();
+        userName=copyUser.getUserName();
+        addToList(this);
     }
 
 
-    ////////////****** String Function used to save into csv file**********///////////
-    @Override
-    public String toString() {
-        StringBuilder buffer= new StringBuilder(ID + "," + Name + "," + userName);
+    ///////////////////////////////////////////////////////
 
+    @Override
+    public String toString(){
+        StringBuilder buffer= new StringBuilder(ID + "," + userName + "," + FirstName + "," + LastName);
         for (int i = 0; i <noFriends ; i++) {
             buffer.append(',').append(Friends.get(i).ID);
         }
@@ -80,8 +144,32 @@ public class User {
         return ID;
     }
 
-    public String getName() {
-        return Name;
+    public String getFirstName() {
+        return FirstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getName(){
+        return FirstName + " " + LastName;
+    }
+
+    public void setFirstName(String name) {
+        FirstName = name;
+    }
+    public void setLastName(String name){
+        LastName = name;
+    }
+
+    public void setName(String name) throws Exception {
+        StringTokenizer nameToken = new StringTokenizer(name);
+        try{
+            FirstName = nameToken.nextToken(" ");
+            LastName = nameToken.nextToken();
+        }catch(Exception ex){
+            throw new Exception("Invalid Name");
+        }
     }
 
     public void setUserName(String name) {
@@ -90,10 +178,6 @@ public class User {
 
     public String getUserName() {
         return userName;
-    }
-
-    public void setName(String name) {
-        Name = name;
     }
 
     public LinkedList<User> getFriends() {
@@ -108,6 +192,11 @@ public class User {
     public int getNoFriends() {
         return noFriends;
     }
+
+    public gender getGender(){return Gender;}
+    public void setGender(gender g){Gender=g;}
+    public Date getBirthDate(){return birthDate;}
+    public void setBirthDate(Date d){birthDate=d;}
 
 
     ////////*********other functions***********//////////////
@@ -127,6 +216,19 @@ public class User {
     public boolean isFriend(User user)
     {
         return Friends.contains(user);
-
     }
+
+    public void delete()
+    {
+        FirstName=null;
+        LastName=null;
+        userName=null;
+        noFriends=0;
+        Friends=null;
+        Gender=null;
+        birthDate=null;
+        allUsersID.remove(this);
+        allUsersName.remove(this);
+    }
+
 }
--- usefulFunctions.java
+++ usefulFunctions.java
@@ -1,59 +1,75 @@
 package sample;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import static sample.User.*;
+
 public class usefulFunctions {
 
-    static int userIdBinarySearch(User arr[], int l, int r, int id)
-            //returns the index if available, or -1 if unavailable
-    {
-        if (r>=l)
-        {
-            int mid = l + (r - l)/2;
-
-            if (arr[mid].getID() == id)
-                return mid;
-            else if (arr[mid].getID() > id)
-                return userIdBinarySearch(arr, l, mid-1, id);
-            else return userIdBinarySearch(arr, mid+1, r, id);
-        }
-        else        return -1;
+    static int userIDBinarySearch(ArrayList<User> arr, int start, int end, int ID){
+        //returns the index if found, and -1 if not
+        if(start >= end) return -1;
+        int mid = (start + end - 1)/2;
+        if(ID == arr.get(mid).getID()) return mid;
+        else if(ID < arr.get(mid).getID()) return userIDBinarySearch(arr,start,mid,ID);
+        else return userIDBinarySearch(arr,mid+1,end,ID);
     }
 
-    static int IndexAddingByIdSearch(int arr[], int l, int r, int id, boolean rightWay)
-            //returns -1 if available, and the right index to add the item if available
-            //rightWay ---> checks if I came from right or left side in previous iteration
-    {
-        if (r>=l)
-        {
-            int mid = l + (r - l)/2;
-            if (arr[mid] == id)
-                return -1;
-            else if (arr[mid] > id)
-                return IndexAddingByIdSearch(arr, l, mid-1, id, false);
-            else return IndexAddingByIdSearch(arr, mid+1, r, id, true);
+    static int ID_index(ArrayList<User> arr, int start, int end, int ID) {
+        //returns -1 if found, or the index to add if not
+        if (start >= end) {
+            return start;
         }
+        int mid = (start + end - 1) / 2;
+        if (ID == arr.get(mid).getID()) return -1;
+        else if (ID <= arr.get(mid).getID()) return ID_index(arr, start, mid, ID);
+        else return ID_index(arr, mid + 1, end, ID);
+    }
+
+    static int userNameBinarySearch(ArrayList<User> arr, int start, int end, String name){
+        if(start >= end) return -1;
+        int mid = (start + end - 1)/2;
+        int compare = name.compareTo(arr.get(mid).getUserName());
+        if(compare == 0) return mid;
+        else if(compare < 0) return userNameBinarySearch(arr,start,mid,name);
+        else return userNameBinarySearch(arr,mid+1,end,name);
+    }
+    static int name_index(ArrayList<User> arr, int start, int end, String username) {
+        if (start >= end) {
+            return start;
+        }
+        int mid = (start + end - 1) / 2;
+        int compare = username.compareTo(arr.get(mid).getUserName());
+        if (compare == 0) return -1;
+        else if (compare < 0) return name_index(arr, start, mid, username);
+        else return name_index(arr, mid + 1, end, username);
+    }
+
+
+    static void addToList(User user)
+    {
+        String name=user.getFirstName()+" "+user.getLastName();
+        int index;
+        index=userNameBinarySearch(allUsersName,0,allUsersName.size()-1,name);
+        if(index!=-1) allUsersName.add(index, user);
         else
         {
-            if(rightWay==false)return l;
-            else return r;
+                index=name_index(allUsersName,0,allUsersName.size()-1,name);
+                allUsersName.add(index, user);
+        }
+        int id=user.getID();
+        index=userIDBinarySearch(allUsersID,0,allUsersID.size()-1,id);
+        if(index!=-1) allUsersID.add(index, user);
+        else
+        {
+            index=ID_index(allUsersID,0,allUsersID.size()-1,id);
+            allUsersID.add(index, user);
         }
     }
-
-    /*
-    static int userNameBinarySearch(User arr[], int l, int r, String name){}
-
-    static int IndexAddingByNameSearch(int arr[], int l, int r, String name, boolean rightWay){}
-
-    boolean compareStrings(String s1, String S2){}
-    */
-
 }
-
-
-
-
-
-
-
 
 
 
