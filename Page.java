e1bdb3f4057095cc74a7c4605b02bac5dda03223
Ali Badawy <alibadawy9634@gmail.com>
2018-05-01 10:01:07 +0200

    Delete Page.java

--- Page.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import javafx.scene.Node;
-import javafx.scene.layout.Pane;
-
-abstract public class Page {
-    abstract Pane get_page_layout();
-    abstract Pane get_page_layout(User user);
-}
