7b40bb49a4ce673b20f47ce62b91a21d5f8b8749
Ali Badawy <alibadawy9634@gmail.com>
2018-05-01 18:26:40 +0200

    Add files via upload

--- sample/PostsDataBase.java
+++ sample/PostsDataBase.java
@@ -2,15 +2,19 @@ package sample;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 import static sample.User.allUsersID;
 import static sample.User.allUsersName;
+import static sample.usefulFunctions.userNameBinarySearch;
 
 public class PostsDataBase {
     private static JSONObject Root = new JSONObject();
@@ -60,6 +64,33 @@ public class PostsDataBase {
             System.out.print(e.toString());
         }
 
+
+    }
+    public static void ReadPost(){
+        try  {
+            Scanner input = new Scanner(file);
+            StringBuilder JSONin = new StringBuilder();
+            while (input.hasNextLine()) {
+                JSONin.append(input.nextLine());
+            }
+            //start parsing
+            JSONParser parser = new JSONParser();
+            JSONObject tempRoot = (JSONObject) parser.parse(JSONin.toString());
+            //creating the posts array
+            JSONArray tempPostArray = (JSONArray) tempRoot.get("Posts");
+            for (int i=0;i<tempPostArray.size();i++){
+                JSONObject tempPost= (JSONObject) tempPostArray.get(i);
+               // System.out.println(tempPost.get("Post Owner"));
+               String ownerUserName= (String) tempPost.get("Post Owner");
+               int indexOfOwner=userNameBinarySearch(allUsersID,0,allUsersID.size(),ownerUserName);
+                System.out.println((allUsersID.get(i).getUserName()));
+            }
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- sample/UsersDataBase.java
+++ sample/UsersDataBase.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 import static sample.User.allUsersID;
 import static sample.User.allUsersName;
 import static sample.usefulFunctions.addToList;
+import static sample.usefulFunctions.userNameBinarySearch;
 
 
 public class UsersDataBase {
@@ -36,11 +37,11 @@ public class UsersDataBase {
             userObject.put("Gender", allUsersID.get(i).getGender_string());
             userObject.put("Birth Date", allUsersID.get(i).getBirthDate().toString());
             userObject.put("Number of Friends", allUsersID.get(i).getNoFriends());
-            userObject.put("List Of Friends", allUsersID.get(i).getFriends().toString());
+            userObject.put("List Of Friends", allUsersID.get(i).getFriends());
             userObject.put("Number of Groups", allUsersID.get(i).getNoGroups());
             userObject.put("List of Groups", allUsersID.get(i).getGroups().toString());
             userObject.put("Number Of Posts", allUsersID.get(i).getNoPosts());
-         //   userObject.put("List of Posts", allUsersID.get(i).getPosts().toString());
+            //   userObject.put("List of Posts", allUsersID.get(i).getPosts().toString());
 
             Users.add(userObject);
 
@@ -65,7 +66,7 @@ public class UsersDataBase {
 
     public static void ReadFromFile() {
 
-        System.out.println("reading");
+        //System.out.println("reading");
         try {
             Scanner input = new Scanner(file);
             StringBuilder JSONin = new StringBuilder();
@@ -81,53 +82,42 @@ public class UsersDataBase {
             JSONArray tempUserArray = (JSONArray) tempRoot.get("Users");
 
             for (int i = 0; i < tempUserArray.size(); i++) {
-                JSONObject tempUser= (JSONObject) tempUserArray.get(i);
-                String [] dateArr=tempUser.get("Birth Date").toString().split("-");
+                JSONObject tempUser = (JSONObject) tempUserArray.get(i);
+                String[] dateArr = tempUser.get("Birth Date").toString().split("-");
                 int year = Integer.parseInt(dateArr[0]);
                 int month = Integer.parseInt(dateArr[1]);
-                int day= Integer.parseInt(dateArr[2]);
-                String First_Name=tempUser.get("First Name").toString();
-                String User_Name=tempUser.get("User Name").toString();
-                String Last_Name=tempUser.get("Last Name").toString();
+                int day = Integer.parseInt(dateArr[2]);
+                String First_Name = tempUser.get("First Name").toString();
+                String User_Name = tempUser.get("User Name").toString();
+                String Last_Name = tempUser.get("Last Name").toString();
 
-                String Gender=tempUser.get("Gender").toString();
+                String Gender = tempUser.get("Gender").toString();
 
-                try      {
-                   addToList(new User(User_Name,First_Name,Last_Name,Gender,LocalDate.of(year,month,day)));
-                }catch(Exception ignored){}
-            }
-            //for loop to go over all of the users
-            for(int i = 0 ; i <allUsersID.size();i++){
-                JSONObject tempUser =  (JSONObject) tempUserArray.get(i);
-                //check to see if json member is the same as the member in allUsersID
-                if ((allUsersID.get(i)).getUserName()==tempUser.get("User Name")){
-                    //get the JSONArray of friends in current member
-                    JSONArray UserFriends = (JSONArray) tempUser.get("List Of Friends");
-                    for(int j = 0 ; j < UserFriends.size();i++){
-                        //add the list of friends after finding the user that match the user name
-                        for(int k = 0 ; k < allUsersID.size();k++){
-                            JSONObject ju=(JSONObject) UserFriends.get(j);
-                            User temp =allUsersID.get(k);
-                        if(temp.getUserName()==ju.get("User Name")){
-                            allUsersID.get(i).addFriend(temp);
-                        }
-
-                    }
-                    }
+                try {
+                    addToList(new User(User_Name, First_Name, Last_Name, Gender, LocalDate.of(year, month, day)));
+                } catch (Exception ignored) {
                 }
             }
+            //for loop to go over all of the users
+            for (int i = 0; i < allUsersID.size(); i++) {
+                //tempUserArray contain all the users
+                JSONObject tempUser = (JSONObject) tempUserArray.get(i);
+                //check to see if json member is the same as the member in allUsersID
+                if ((allUsersID.get(i)).getUserName() == tempUser.get("User Name")){
+                    //get friends array
+                    JSONArray tempFriendsList= (JSONArray) tempUser.get("List Of Friends");
+                //for loop for all friends
+                for (int j = 0; j < tempFriendsList.size(); j++){
+                    String friendString = (String) tempFriendsList.get(j);
+                    //get the index of the friend
+                    int indexOfFriend=userNameBinarySearch(allUsersID,0,allUsersID.size(),friendString);
+                    //add friend
+                    allUsersID.get(i).addFriend(allUsersID.get(indexOfFriend));
+                 }
 
-         /**   for(int i = 0 ; i < allUsersID.size();i++){
-                JSONObject tempUser =  (JSONObject) tempUserArray.get(i);
-                if ((allUsersID.get(i)).getUserName()==tempUser.get("User Name")){
-                    //get the JSONArray of Posts in current member
-                    JSONArray UserPosts = (JSONArray) tempUser.get("List of Posts");
-                    for(int j = 0 ; j < UserPosts.size();i++){
-                       // (allUsersID.get(i)).addPost(/*string)
-                    }
-                     }
+            }
+            }
 
-            }**/
 
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
